17e8eb0a2bc0e0655194421107c0bae96c17c9ce
david.berga <david.berga@eurecat.org>
2023-06-20 17:18:26 +0200

    added download process_data and train.sh bash scripts

---
+++ process_data.sh
@@ -0,0 +1,5 @@
+#ns-process-data images --data data/sagrada/images --output-dir data/sagrada --no-gpu;
+ns-process-data video --data data/rafa/IMG_8737.MOV --output-dir data/rafa --no-gpu;
+ns-process-data video --data data/rafa/IMG_8743.MOV --output-dir data/rafa --no-gpu;
+ns-process-data video --data data/rafa/IMG_8749.MOV --output-dir data/rafa --no-gpu;
+
---
+++ train.sh
@@ -0,0 +1,45 @@
+if [ -z $1 ]
+then
+export CUDA_VISIBLE_DEVICES=0
+else
+export CUDA_VISIBLE_DEVICES=$1
+fi
+if [ -z $2 ]
+then
+export MODEL="nerfacto"
+else
+export MODEL=$2
+fi
+if [ -z $3 ]
+then
+export DATASET="data/nerfstudio/kitchen"
+else
+export DATASET=$3
+fi
+
+# RUN
+ns-train $MODEL --data $DATASET --vis wandb;
+
+############################################
+#export CUDA_VISIBLE_DEVICES=0;
+#export MODEL='nerfacto';
+#ns-train nerfacto --data data/nerfstudio/poster --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/bww_entrance --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/campanile --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/desolation --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/library --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/redwoods2 --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/storefront --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/vegetat --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/ion --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/Egypt --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/person --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/kitchen --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/plane --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/dozer --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/floating-tree --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/aspen --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/stump --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/sculpture --vis wandb;
+#ns-train nerfacto --data data/nerfstudio/Giannini-Hall --vis wandb;
+
