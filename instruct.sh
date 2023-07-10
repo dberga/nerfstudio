4a98838f4d5a82accd68ed5540c937fe549e646a
david.berga <david.berga@eurecat.org>
2023-07-10 14:28:29 +0200

    updated instruct.sh, correct use of quotation marks for prompt

--- instruct.sh
+++ instruct.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ -z $1 ]
 then
 export CUDA_VISIBLE_DEVICES=0
@@ -16,11 +17,11 @@ export DATASET="data/nerfstudio/kitchen"
 else
 export DATASET=$3
 fi
-if [ -z $4 ]
+if [ -z "$4" ]
 then
 export PROMPT="replace humans by aliens"
 else
-export PROMPT=$4
+export PROMPT="$4"
 fi
 if [ -z $5 ]
 then
@@ -35,6 +36,8 @@ else
 export ISCALE=$6
 fi
 export SCENE=$(echo $(basename $DATASET))
+
+echo $PROMPT
 CKPT_PATH=$(ls outputs/$SCENE/$MODEL/*/*/*.ckpt | sort -n | tail -n 1)
 MODEL_PATH=$(dirname $(dirname $CKPT_PATH))
 CKPT_DATE=$(basename $MODEL_PATH)
@@ -42,7 +45,6 @@ CFG_PATH=$MODEL_PATH/config.yml
 OUTPUT_PATH=$MODEL_PATH/results.json
 
 # RUN
-
-ns-train in2n --data $DATASET --load-dir $MODEL_PATH --pipeline.prompt { $PROMPT } --pipeline.guidance-scale $GSCALE --pipeline.image-guidance-scale $ISCALE
-echo "ns-train in2n --data $DATASET --load-dir $MODEL_PATH --pipeline.prompt { $PROMPT } --pipeline.guidance-scale $GSCALE --pipeline.image-guidance-scale $ISCALE"
+echo "ns-train in2n --data $DATASET --load-dir $MODEL_PATH --pipeline.prompt '$PROMPT' --pipeline.guidance-scale $GSCALE --pipeline.image-guidance-scale $ISCALE --vis viewer"
+ns-train in2n --data $DATASET --load-dir $MODEL_PATH --pipeline.prompt '$PROMPT' --pipeline.guidance-scale $GSCALE --pipeline.image-guidance-scale $ISCALE --vis viewer
 
