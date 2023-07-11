0e28aa0454aa3f224786e5b11da2e9528baa7880
david.berga <david.berga@eurecat.org>
2023-07-11 13:11:43 +0200

    added whole_process.sh and whole_train.sh to benchmark all folders and subfolders

---
+++ whole_train.sh
@@ -0,0 +1,27 @@
+if [ -z $1 ]
+then
+export CUDA_VISIBLE_DEVICES=0
+else
+export CUDA_VISIBLE_DEVICES=$1
+fi
+if [ -z $2 ]
+then
+export MODEL="nerfacto"
+else
+export MODEL=$2
+fi
+if [ -z $3 ]
+then
+export VIS="wandb"
+else
+export VIS=$3
+fi
+
+
+for FOLDER in data/*; 
+do
+sh all_train.sh $CUDA_VISIBLE_DEVICES $FOLDER $MODEL $VIS
+done
+
+
+
