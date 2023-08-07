d5947100001ac2491bd31dc02fb996f99ebcc3ab
david.berga <david.berga@eurecat.org>
2023-08-07 12:57:38 +0200

    removed old processes

--- test_DissenyHub.sh
+++
@@ -1,9 +0,0 @@
-export CUDA_VISIBLE_DEVICES=1;
-#ns-process-data video --data data/Museu-DissenyHub/cobi_metal/IMG_9382.MOV --output-dir data/Museu-DissenyHub/cobi_metal;
-#ns-train nerfacto --data data/Museu-DissenyHub/cobi_metal --vis wandb;
-#ns-process-data video --data data/Museu-DissenyHub/bustos_porcelana/IMG_9409.MOV --output-dir data/Museu-DissenyHub/bustos_porcelana;
-#ns-train nerfacto --data data/Museu-DissenyHub/bustos_porcelana --vis wandb;
-ns-process-data video --data data/Museu-DissenyHub/maniquis_terciopelo/IMG_9442.MOV --output-dir data/Museu-DissenyHub/maniquis_terciopelo;
-ns-train nerfacto --data data/Museu-DissenyHub/maniquis_terciopelo --vis wandb;
-ns-process-data video --data data/Museu-DissenyHub/platos_laton/IMG_9392.MOV --output-dir data/Museu-DissenyHub/platos_laton;
-ns-train nerfacto --data data/Museu-DissenyHub/platos_laton --vis wandb;
--- test_MAC.sh
+++
@@ -1,5 +0,0 @@
-export CUDA_VISIBLE_DEVICES=2,3;
-ns-process-data video --data data/Museu-MAC/casco_ceramico/IMG_9158.MOV --output-dir data/Museu-MAC/casco_ceramico --no-gpu;
-ns-train nerfacto --data data/Museu-MAC/casco_ceramico --vis wandb;
-ns-process-data video --data data/Museu-MAC/jarrones_vidrio/IMG_9198.MOV --output-dir data/Museu-MAC/jarrones_vidrio --no-gpu;
-ns-train nerfacto --data data/Museu-MAC/jarrones_vidrio --vis wandb;
