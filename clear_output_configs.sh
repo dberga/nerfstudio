3d5289fe2d456b353cd9ddbd927d52dcf56dbea0
david.berga <david.berga@eurecat.org>
2023-08-28 13:41:34 +0200

    script for clearing empty output files

--- clear_output_configs.sh
+++ clear_output_configs.sh
@@ -9,9 +9,29 @@ then
 echo "$DATE checkpoint exists, all ok here"
 else
 echo "$DATE checkpoint does not exist, removing empty folder (and config.yml)"
-rm -rf $DATE/config.yml
+#rm -rf $DATE/config.yml
+rm -rf $DATE
 fi
 done
+if [ -z "$(ls $MODEL)" ]
+then
+echo $MODEL ended up empty, removing whole folder.
+rm -rf $MODEL
+fi
 done
+if [ -z "$(ls $SCENE)" ]
+then
+echo $SCENE ended up empty, removing whole folder
+rm -rf $SCENE
+fi
+
+if [ "$(ls -A $SCENE | wc -l)" -eq 1 ]
+then
+if [ -e $SCENE/benchmark_$(basename $SCENE).csv ]
+then
+echo $SCENE only has empty benchmark, removing whole folder
+rm -rf $SCENE
+fi
+fi
 done
 
