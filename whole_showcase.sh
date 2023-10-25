479978a34ae5923a554fe8e8ed56c0ca16d48cc0
david.berga <david.berga@eurecat.org>
2023-10-25 12:30:06 +0200

    added whole_showcase.sh and all_showcase.sh scripts for processing input data as video and gif for demos showcase

---
+++ whole_showcase.sh
@@ -0,0 +1,20 @@
+if [ -z $1 ]
+then
+export CUDA_VISIBLE_DEVICES=0
+else
+export CUDA_VISIBLE_DEVICES=$1
+fi
+if [ -z $2 ]
+then
+export OVERWRITE=false
+else
+export OVERWRITE=$2
+fi
+
+for FOLDER in data/*; 
+do
+sh all_showcase.sh $CUDA_VISIBLE_DEVICES $FOLDER $OVERWRITE
+done
+
+
+
